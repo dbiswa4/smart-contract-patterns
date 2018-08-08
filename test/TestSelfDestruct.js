022a21621ed766e4df6389271c0076d369fdadde
dbiswas <dipankar.biswas@oath.com>
2018-08-07 21:13:21 -0700

    Self Destruct contract test fixed

--- test/TestSelfDestruct.js
+++ test/TestSelfDestruct.js
@@ -1,27 +1,29 @@
 //var SelfDestruct = artifacts.require("./SelfDestruct.sol");
-var SelfDestruct = artifacts.require('SelfDestruct.sol');
+var SelfDestruct = artifacts.require('SelfDestruct');
 
 contract('Test SelfDestruction Contract', function(accounts) {
 
-    var selfDestruct;
 
-    if('Test Destruction Function ', function(){
+    it('Test Destruction Function ', function(){
+
+        var selfDestruct;
+
         //1. Get the deployed contract instance
         return SelfDestruct.deployed().then(function(instance) {
             //2. Set the value
             selfDestruct = instance;
-            selfDestruct.setName("Mosquito");
+            selfDestruct.setName("Mosquito", {from:accounts[0]});
             //3. Get the value
             return selfDestruct.name.call();
         }).then(function(result){
             //4. Print the received value to console
             console.log("Name : ", result);
             //5. Call kill
-            return selfDestruct.killContract();
-        }).then(function(result){
+            return selfDestruct.killContract({from:accounts[0]});
+        }).then(function(){
             console.log("Contract Destroyed");
             // This call will throw an excepion as contract is destroyed
             selfDestruct.setName("Rat");
-        })
+        });
     });
 })
